0e10211ba4473500869c751c04dd2b690097a7b3
JasonEricZhan <jan11781202@gmail.com>
2017-09-09 20:03:55 +0800

    Update main.c

--- flattest_lowest_hillclimbing/main.c
+++ flattest_lowest_hillclimbing/main.c
@@ -5,7 +5,6 @@
 //  Created by Eric on 2017/4/19.
 //  Copyright © 2017年 Eric. All rights reserved.
 //
-
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
@@ -23,17 +22,17 @@ int main(int argc, const char * argv[]) {
     scanf(" %2i",&n);
     
     //allocate dynamic array D[n][n]
-    float **D_2D=NULL;
-    D_2D = (float**)malloc(sizeof(float*)*n);//allocate n rows of space to store pointers
-    if(!D_2D )
+    float **cost_2D=NULL;
+    cost_2D = (float**)malloc(sizeof(float*)*n);//allocate n rows of space to store pointers
+    if(!cost_2D )
     {
         printf("Fail to allocate memory");
         exit(1);//unusual exit out the program
     }
     for(int i=0;i<n;i++)
     {
-        D_2D[i]=(float*)malloc(sizeof(float)*n);//columns of i-th row
-        if(!D_2D[i])
+        cost_2D[i]=(float*)malloc(sizeof(float)*n);//columns of i-th row
+        if(!cost_2D[i])
         {
             printf("Fail to allocate memory");
             exit(1);//unusual exit out the program
@@ -41,17 +40,17 @@ int main(int argc, const char * argv[]) {
     }
     
     //allocate dynamic array P[n][n]
-    int **P_2D=NULL;
-    P_2D = (int**)malloc(sizeof(int*)*n);//allocate n rows of space to store pointers
-    if(!P_2D )
+    int **path_2D=NULL;
+    path_2D = (int**)malloc(sizeof(int*)*n);//allocate n rows of space to store pointers
+    if(!path_2D )
     {
         printf("Fail to allocate memory");
         exit(1);//unusual exit out the program
     }
     for(int i=0;i<n;i++)
     {
-        P_2D[i]=(int*)malloc(sizeof(int)*n);//columns of i-th row
-        if(!P_2D[i])
+        path_2D[i]=(int*)malloc(sizeof(int)*n);//columns of i-th row
+        if(!path_2D[i])
         {
             printf("Fail to allocate memory");
             exit(1);//unusual exit out the program
@@ -88,12 +87,12 @@ int main(int argc, const char * argv[]) {
         }
     }
     getchar();
-    dp(n,D_2D,height_2D,P_2D);//dynamic programming
+    dp(n,cost_2D,height_2D,path_2D);//dynamic programming
     printf("\n");
     //use stack to reverse the order of output
     list *Top=NULL;  //intend to be safe
     Top=createnode();// create the node
-    Top=record(P_2D,n-1,n-1,Top);//trace back from end point and return the position of the top of stack
+    Top=record(path_2D,n-1,n-1,Top);//trace back from end point and return the position of the top of stack
     do
     {
         int j=-1;int i=-1;
@@ -111,18 +110,18 @@ int main(int argc, const char * argv[]) {
     //free allocated array
     for(int i=0;i<n;i++)
     {
-        free(D_2D[i]);
-        D_2D[i]=NULL;
+        free(cost_2D[i]);
+        cost_2D[i]=NULL;
     }
-    free(D_2D);
-    D_2D=NULL;
+    free(cost_2D);
+    cost_2D=NULL;
     for(int i=0;i<n;i++)
     {
-        free(P_2D[i]);
-        P_2D[i]=NULL;
+        free(path_2D[i]);
+        path_2D[i]=NULL;
     }
-    free(P_2D);
-    P_2D=NULL;
+    free(path_2D);
+    path_2D=NULL;
     for(int i=0;i<n;i++)
     {
         free(height_2D[i]);
@@ -133,8 +132,8 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-
-
-
-
+    
+    
+    
+    
 
