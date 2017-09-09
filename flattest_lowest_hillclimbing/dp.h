8789d30d97938ab21397cd8b8f7e60035cace0c5
JasonEricZhan <jan11781202@gmail.com>
2017-09-09 21:24:36 +0800

    Update dp.h

--- flattest_lowest_hillclimbing/dp.h
+++ flattest_lowest_hillclimbing/dp.h
@@ -1,7 +1,7 @@
 void dp(int length,float **cost,float **height,char **path);
 
-float diff(int j1,int i1,int j2,int i2,float **s);
+float diff(int col1,int row1,int col2,int row2,float **height);
 
 float min(float a,float b);
 
-int slope_check(int j,int i);
+int slope_check(int col,int row);
