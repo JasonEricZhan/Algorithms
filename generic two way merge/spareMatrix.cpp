35a806895585532d87667de9f25652f250889834
Chan Yao Chun(Eric) <jan11781202@gmail.com>
2017-10-20 04:12:36 -0500

    Update spareMatrix.cpp

--- generic two way merge/spareMatrix.cpp
+++ generic two way merge/spareMatrix.cpp
@@ -58,7 +58,6 @@ void multiply_wrapper(int &aPtr,int &bPtr,matrixTerm *arr_a,matrixTerm *arr_b,in
     }
 }
 
-//store it to the std function, then can reuse it
 function<void(int &aPtr,int &bPtr,matrixTerm *arr_a,matrixTerm *arr_b,int &sum,int flag)> sp_func =multiply_wrapper;
 
 
@@ -94,11 +93,10 @@ spMatrix spMatrix::mul(spMatrix b)
     {
         spMatrix newMatrix(rows*b.getCols(),rows,b.getCols(),0);
         
-        //change to column major
+        
         matrixTerm* colbsmArray=b.colMajor();
         
-        
-        //store the changing row ,column boundary for a and b
+
         vector<int> aPtrbound;
         for(int i=0;i<term_num-1;i++)
         {
@@ -132,45 +130,40 @@ spMatrix spMatrix::mul(spMatrix b)
         //matrix a  by row major,matrix b by column major, operation is a*b
         while(aPtr<term_num)
         {
-            while(bPtr<=bTerms)//multiply all the b's element by a's one row
-            {//trick:bPtr going to bTerms is to put the result of last summation operation in storeSum
-                
-                if(aPtr>=aPtrbound[la] or bPtr>=bPtrbound[lb])//end of row
+            while(bPtr<=bTerms)
+            {   if(aPtr>=aPtrbound[la] or bPtr>=bPtrbound[lb])//end of row
                 {
                     newMatrix.storeSum(sum,currRow,currCol);
                     sum=0;
-                    aPtr=aStarter;//initialize to the same row, ready to start again
-                    if(bPtr==bTerms)//prevent some ambiguous
+                    aPtr=aStarter;
+                    if(bPtr==bTerms)
                     {
                         break;
                     }
-                    //put in front of while loop for saving a little time
                     while(colbsmArray[bPtr].col==currCol)
-                        //The b pointer isn't point to the next column elements?
                     {
-                        bPtr++;//if doesn't point to the next column elements, the b pointer has to go to next column
+                        bPtr++;
                     }
                     currCol=colbsmArray[bPtr].col;
                 }
                 else
                 {
-                    //set the value of starters and boundary
                     list spMerge(aPtrbound[la], bPtrbound[lb],aPtr,bPtr);
                     spMerge.merge(smArray, colbsmArray,sp_func,sum);
                     aPtr=spMerge.get_lPtr();bPtr=spMerge.get_rPtr();
                     lb++;
                 }
             }
-            la++; //go to next boundary label for a
-            lb=0;//go back to first boundary label for b
-            while(smArray[aPtr].row==currRow)//complete a's one row multiply
+            la++; 
+            lb=0;
+            while(smArray[aPtr].row==currRow)
             {
-                aPtr++;    //go to next next row for a
+                aPtr++;   
             }
-            aStarter=aPtr;//record the next column position for a
-            currRow=smArray[aPtr].row;//record  next row for a 
-            currCol=colbsmArray[0].col;//record the column of first element for b
-            bPtr=0;//go to the first element for b
+            aStarter=aPtr;
+            currRow=smArray[aPtr].row;
+            currCol=colbsmArray[0].col;
+            bPtr=0;
         }
         
         if(newMatrix.getCapacity()>newMatrix.getTerms())//clean out some unnecessary space
